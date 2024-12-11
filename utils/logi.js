4ec1a72468e6222fc8993c51e0bdfd1f6ad7620e
Atta Rasul <atta@devgiant.io>
2024-12-11 20:22:27 +0500

    changes

--- utils/logi.js
+++ utils/logi.js
@@ -8,18 +8,22 @@ var config = require('../config.js');
 //receive a message like console.log i can call it with comma separated values
 function log(...message) {
   if (config.env === 'production') {
-    console.log(path.join(app.getPath('userData'), 'logs.txt'));
-    const logFilePath = path.join(app.getPath('userData'), 'logs.txt'); 
-    const timestamp = new Date().toISOString();
-    //message may be object
-    if (message.length === 1 && typeof message[0] === 'object') {
-      message = [JSON.stringify(message[0], null, 2)];
+    try {
+      console.log(path.join(app.getPath('userData'), 'logs.txt'));
+      const logFilePath = path.join(app.getPath('userData'), 'logs.txt');
+      const timestamp = new Date().toISOString();
+      //message may be object
+      if (message.length === 1 && typeof message[0] === 'object') {
+        message = [JSON.stringify(message[0], null, 2)];
+      }
+      var messages = message.join(' ');
+      const logMessage = `${timestamp} - ${messages}\n`;
+
+      // Append log to file
+      fs.appendFileSync(logFilePath, logMessage);
+    } catch (e) {
+      console.log(message.join(' '));
     }
-    var messages = message.join(' ');
-    const logMessage = `${timestamp} - ${messages}\n`;
-    
-    // Append log to file
-    fs.appendFileSync(logFilePath, logMessage);
   } else {
     console.log(message.join(' '));
   }
