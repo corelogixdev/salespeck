814727972edee06ac8c9e87cb8f464ffb6a286ba
Atta Rasul <atta@devgiant.io>
2025-01-10 19:48:38 +0500

    changtee

--- build/upload.js
+++ build/upload.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const { execSync } = require('child_process');
 const fs = require('fs');
-const path = require('path');
 
-const distDir = path.join(__dirname, '../dist');
-const files = fs.readdirSync(distDir);
+console.log('Uploading executable...');
+execSync(`curl --verbose --header "PRIVATE-TOKEN: ${process.env.GITLAB_TOKEN}" --upload-file dist/openmenu.exe "https://gitlab.com/api/v4/projects/${process.env.CI_PROJECT_ID}/packages/generic/openmenu/release/openmenu.exe"`, {stdio: 'inherit'});
 
-files.forEach(file => {
-  const filePath = path.join(distDir, file);
-  console.log(`Uploading ${file}...`);
-  execSync(`curl --verbose --header "PRIVATE-TOKEN: ${process.env.GITLAB_TOKEN}" --upload-file ${filePath} "https://gitlab.com/api/v4/projects/${process.env.CI_PROJECT_ID}/packages/generic/openmenu/release/${file}"`, {stdio: 'inherit'});
-});
+console.log('Uploading latest.yml...');
+if (!fs.existsSync('dist/latest.yml')) {
+  console.error('Error: latest.yml file is missing in the dist directory.');
+  process.exit(1);
+}
+execSync(`curl --verbose --header "PRIVATE-TOKEN: ${process.env.GITLAB_TOKEN}" --upload-file dist/latest.yml "https://gitlab.com/api/v4/projects/${process.env.CI_PROJECT_ID}/packages/generic/openmenu/release/latest.yml"`, {stdio: 'inherit'});
 
 console.log('Upload complete.');
